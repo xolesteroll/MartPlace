05a90e774d71ec4bdbbc4668decedc31427a4e37
xolesteroll <xol2dem@gmail.com>
2020-07-10 13:18:40 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-    
+    $('.weekly__slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="prev arrow"></button>',
+        fade: true,
+        infinite: true
+    });
 });
