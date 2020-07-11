f65226e103f7732e800fa140d27e823fbc6e7bdc
xolesteroll <xol2dem@gmail.com>
2020-07-11 18:23:44 +0300

    added rating stars

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.weekly__slider').slick({
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/arrow-right.png" alt="next arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/arrow-left.png" alt="prev arrow"></button>',
@@ -7,6 +7,20 @@ $(function(){
     });
 
     var mixer = mixitup('.newest__inner');
+
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: '16px',
+        starHeight: '15px',
+        spacing: '3px'
+    });
+
+    $('.rate-star__small').rateYo({
+        rating: 5,
+        starWidth: '13px',
+        starHeight: '12px',
+        spacing: '4px'
+    });
 
 
 
