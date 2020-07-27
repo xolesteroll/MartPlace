101f3639b1c7605d77741abd316cb3eae50912f2
xolesteroll <xol2dem@gmail.com>
2020-07-27 14:49:52 +0300

    created all header dropdowns including megamenu

--- app/js/main.js
+++ app/js/main.js
@@ -72,7 +72,9 @@ $(function () {
 
     $('.header__menu-btn').on('click', function(){
         $('.header__menu-list').slideToggle();
-    })
+    });
+
+
 
 
     var mixer = mixitup('.newest__inner');
